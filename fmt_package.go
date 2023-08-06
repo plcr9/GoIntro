5198246d6c341c604f8448f5de1cd9b1c996be85
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-07 00:35:10 +0100

    Update fmt_package.go

--- fmt_package.go
+++ fmt_package.go
@@ -1,1 +1,10 @@
+package main
 
+import "fmt"
+
+func main() {
+  fmt.Println("Can", "you", "tell", "the", "difference?")
+  fmt.Println("Between", "these", "two", "methods?")
+  fmt.Println("Anything odd about", "the spacing? \n")
+  fmt.Println("Don't worry if you can't spot it, we'll go through this together!")
+}
