fba1666f7bf71396e2fcc0095be50e0b2a05ed4c
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-06 11:48:01 +0100

    Update strings.go

--- strings.go
+++ strings.go
@@ -1,1 +1,9 @@
+package main
 
+import "fmt"
+
+func main() {
+  var favouriteSnack string
+  favouriteSnack = "delicious green broccoli"
+  fmt.Println("My favourite snack is " + favouriteSnack)
+}
