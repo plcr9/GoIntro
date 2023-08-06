35930ade9e8e75543ee4d16d4380d63552254c27
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-06 12:04:24 +0100

    Update building_on_variables.go

--- building_on_variables.go
+++ building_on_variables.go
@@ -1,1 +1,18 @@
+package main
 
+import "fmt"
+
+func main() {
+  coolSneakers := 65.99
+  niceNecklace := 45.50
+
+  var taxCalculation float64
+
+  taxCalculation += coolSneakers
+
+  taxCalculation += niceNecklace
+
+  taxCalculation *= 0.08875
+
+  fmt.Println("Purchase of", coolSneakers + niceNecklace, "with 8.875% sales tax", taxCalculation, "equal to", coolSneakers + niceNecklace + taxCalculation)
+}
