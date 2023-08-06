2079be82e51b8cd577cbdad39c73c319c8c783f6
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-06 12:08:42 +0100

    Update multiple_variable_declaration.go

--- multiple_variable_declaration.go
+++ multiple_variable_declaration.go
@@ -1,1 +1,10 @@
+package main
 
+import "fmt"
+
+func main() {
+  var magicNum, powerLevel int32
+  magicNum = 2048
+  powerLevel = 9001
+
+  fmt.Println("magicNum is:", magicNum, "powerLevel is:", powerLevel)
