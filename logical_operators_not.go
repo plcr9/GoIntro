688a3d8267e57f90955e2d32fa9b10a235be9dc4
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-08 09:35:43 +0100

    Update logical_operators_not.go

--- logical_operators_not.go
+++ logical_operators_not.go
@@ -1,1 +1,13 @@
+package main
 
+import "fmt"
+
+func main() {
+  readyToGo := true
+
+  if !readyToGo {
+    fmt.Println("Start the car!")
+  } else {
+    fmt.Println("What are we waiting for??")
+  }
+}
