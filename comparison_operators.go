ac691167590cd1faea87d3c00d81260fbc0b24db
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-08 09:26:32 +0100

    Update comparison_operators.go

--- comparison_operators.go
+++ comparison_operators.go
@@ -1,1 +1,12 @@
+package main
 
+import "fmt"
+
+func main() {
+  lookCombo := "2-35-19"
+  robAttempt := "1-1-1"
+
+  if lookCombo == robAttempt {
+    fmt.Print("The vault is now opened.")
+  }
+}
