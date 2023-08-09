671792e7bf47b90c07ec847109584da1bc55cf8c
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-09 11:01:01 +0100

    Update reusing_code_with_functions.go

--- reusing_code_with_functions.go
+++ reusing_code_with_functions.go
@@ -1,1 +1,21 @@
+package main
 
+import (
+  "math"
+  "fmt"
+)
+
+func main() {
+  var a, b, c, d float64
+  a = .0214
+  b = 1.02
+  c = 0.312
+  d = 4.001
+
+  a = math.Log2(math.Sqrt(math.Tanh(a)))
+  b = math.Log(math.Sqrt(math.Tanh(b)))
+  c = math.Log(math.Sqrt(math.Tan(c)))
+  d = math.Log2(math.Sqrt(math.Tanh(d)))
+
+  fmt.Println(a, b, c, d)
+}
