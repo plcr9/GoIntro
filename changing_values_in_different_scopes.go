c0d119e94c06d5a5cd4c5e6d5f850b3192322b0f
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-09 11:40:24 +0100

    Update changing_values_in_different_scopes.go

--- changing_values_in_different_scopes.go
+++ changing_values_in_different_scopes.go
@@ -1,1 +1,15 @@
+package main
 
+import "fmt"
+
+func brainwash(saying *string) {
+  *saying = "Beep Boop."
+}
+
+func main() {
+  greeting := "Hello there!"
+
+  brainwash(&greeting)
+
+  fmt.Println("greeting is now:", greeting)
+}
