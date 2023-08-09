90f400261cc36a8de945b9c207442cfce3f6420b
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-09 11:27:48 +0100

    Update multiply_three.go

--- multiply_three.go
+++ multiply_three.go
@@ -1,1 +1,13 @@
+package main
 
+import "fmt"
+
+func multiplyThree(x int, y int, z int) int {
+  return x * y * z
+}
+
+func main() {
+  var product int
+  product = multiplyThree(2, 2, 2)
+  fmt.Println(product)
+}
