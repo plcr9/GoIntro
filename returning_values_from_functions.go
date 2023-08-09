38c94d9ccf8935e7fdbace57b362234d479189cf
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-09 10:54:39 +0100

    Update returning_values_from_functions.go

--- returning_values_from_functions.go
+++ returning_values_from_functions.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+  "fmt"
+  "time"
+)
+
+func isItLateInNewYork() string {
+  var lateMessage string
+  t := time.Now()
+  tz, _ := time.LoadLocation("America/New_York")
+  myHour := t.In(tz).Hour()
+  if myHour < 5 {
+    lateMessage = "Goodness it is late"
+  } else if myHour < 16 {
+    lateMessage = "It's not late at all!"
+  } else if myHour < 19 {
+    lateMessage = "I guess it's getting kind of late"
+  } else {
+    lateMessage = "It's late"
+  }
+
+  return lateMessage
+}
+
+func main() {
+  var myLate string
+  nyLate = isItLateInNewYork()
+  fmt.Println(nyLate)
+}
