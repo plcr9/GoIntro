6723a1b6a19cefa66dffd75eb0c11a17b45a78f8
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-09 11:03:50 +0100

    Update reusing_code_with_functions.go

--- reusing_code_with_functions.go
+++ reusing_code_with_functions.go
@@ -16,10 +16,10 @@ func main() {
   c = 0.312
   d = 4.001
 
-  a = math.Log2(math.Sqrt(math.Tanh(a)))
-  b = math.Log(math.Sqrt(math.Tanh(b)))
-  c = math.Log(math.Sqrt(math.Tan(c)))
-  d = math.Log2(math.Sqrt(math.Tanh(d)))
+  a = specialComputation(a)
+  b = specialComputation(b)
+  c = specialComputation(c)
+  d = specialComputation(d)
 
   fmt.Println(a, b, c, d)
 }
