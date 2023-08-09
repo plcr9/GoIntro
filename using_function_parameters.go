452433dc417b88ad398d19bc9caadeab312d599d
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-09 10:58:09 +0100

    Update using_function_parameters.go

--- using_function_parameters.go
+++ using_function_parameters.go
@@ -1,1 +1,16 @@
+package main
 
+import "fmt"
+
+func computeMarsYears(earthYears int) int {
+  earthDays := earthYears * 365
+  marsYears := earthDays / 687
+  return marsYears
+}
+
+func main() {
+  myAge := 25
+
+  myMartianAge := computeMarsYears(myAge)
+  fmt.Println(myMartianAge)
+}
