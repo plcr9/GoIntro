936660fbb59f2b2c0383868f353f3471a617fb20
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-10 09:55:42 +0100

    Update interstellar_travel.go

--- interstellar_travel.go
+++ interstellar_travel.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 
-func fuleGauge (fuel int) {
+func fuleGauge(fuel int) {
   fmt.Println("You have", fuel, "gallons of fuel left!")
 }
 
-func main() {
-  fuelGauge(7000)
+func calculateFuel(planet string) int {
+  var fuel int
+  switch planet {
+    case "Venus":
+      fuel = 300000
+    case "Mercury":
+      fuel = 500000
+    case "Mars":
+      fuel = 700000
+    default:
+      fuel = 0
+  }
 }
+
