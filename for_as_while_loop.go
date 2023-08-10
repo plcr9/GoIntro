b66faecef4f343ea1069f8f85dd466efafc6a6f7
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-10 14:20:27 +0100

    Update for_as_while_loop.go

--- for_as_while_loop.go
+++ for_as_while_loop.go
@@ -1,1 +1,24 @@
+package main
 
+import (
+	"fmt"
+)
+
+func ask() int {
+	var input int
+	fmt.Print("I am thinking of a number between 1-100: ")
+	fmt.Scan(&input)
+	return input
+}
+
+func main() {
+	var guess int
+
+	for guess != 56 {
+		guess = ask()
+	}
+
+	if guess == 56 {
+		fmt.Println("You are correct! You may go through to the treasure!")
+	}
+}
