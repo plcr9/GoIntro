780e2ec204e2d8e9e58d978fd8dd762f496b59a2
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-10 10:17:33 +0100

    Update comic_mischief.go

--- comic_mischief.go
+++ comic_mischief.go
@@ -1,1 +1,30 @@
+package main
 
+import "fmt"
+
+func main() {
+  var publisher, writer, artist, title string
+  var year, pageNumber int
+  var grade float32
+
+  title = "Mr. GoToSleep"
+  writer = "Tracey Hatchet"
+  artist = "Jewel Tampson"
+  publisher = "DizzyBooks Publishing Inc."
+  year = 1997
+  pageNumber = 14
+  grade = 6.5
+
+  fmt.Println(title, "written by", writer, "drawn by", artist, "is published by", publisher, "in the year", year, "It has a total of:", pageNumber, "pages and this copy has a grade of:", grade)
+  fmt.Println("-------------------------------------")
+
+  title = "Epic Vol. 1"
+  writer = "Ryan N. Shawn"
+  artist = "Phoebe Paperclips"
+  publisher = "DizzyBooks Publishing Inc."
+  year = 2013
+  pageNumber = 160
+  grade = 9.0
+  
+  fmt.Println(title, "written by", writer, "drawn by", artist, "is published by", publisher, "in the year", year, "It has a total of:", pageNumber, "pages and this copy has a grade of:", grade)
+}
