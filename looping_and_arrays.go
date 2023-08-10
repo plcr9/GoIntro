d9ea6c8986d99e606826694e668b49a8cbda89b1
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-10 14:41:57 +0100

    Update looping_and_arrays.go

--- looping_and_arrays.go
+++ looping_and_arrays.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+  "fmt"
+)
+
+func main() {
+  menu := []string{"Hamburgers", "Cheeseburgers", "Fries"}
+  fmt.Println("The menu:")
+  for number, item := range menu {
+    fmt.Println(number, ":", item)
+  }
+  orders := map[string]string{
+    "John": "Cheeseburgers",
+    "Janet": "Hamburgers",
+    "Jordan": "Fries",
+  }
+  orders["James"] = "Chicken Sandwich"
+
+    fmt.Println("\nThe friend's orders:")
+
+  for name, order := range orders {
+    fmt.Println(name, "wants some", order)
+  }
+}
