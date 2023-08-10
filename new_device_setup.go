a1a2c411eb589ba8573ba45da238e8f52445b334
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-10 14:02:14 +0100

    Update new_device_setup.go

--- new_device_setup.go
+++ new_device_setup.go
@@ -13,4 +13,6 @@ func main() {
   fmt.Println("What is your age?")
   fmt.Scan(&age)
   fmt.Printf("%s is %d years old.\n", name, age)
+  newMessage := fmt.Sprintf("Hi I'm %s and I am %d years old.", name, age)
+  fmt.Println(newMessage)
 }
