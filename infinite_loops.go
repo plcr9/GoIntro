ba033c05bf0a99c519fa5f8957dcce514c3dd347
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-10 14:25:34 +0100

    Update infinite_loops.go

--- infinite_loops.go
+++ infinite_loops.go
@@ -1,1 +1,17 @@
+package main
+
+import (
+  "fmt"
+)
+
+var number int
+
+func count() {
+  var input int
+  fmt.Print("Number of guests: ")
+  fmt.Scan(&input)
+  number = number + input
+  fmt.Println("Total guests:", number)
+}
+
 
