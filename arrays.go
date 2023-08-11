20f3097f90d3b32b013b40d070203063aec8777c
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-11 10:55:47 +0100

    Update arrays.go

--- arrays.go
+++ arrays.go
@@ -1,1 +1,12 @@
+package main
 
+import "fmt"
+
+func main() {
+  var playerScores[4]int
+  playerScores[0] = 43
+  playerScores[1] = 7
+  playerScores[2] = 32
+  playerScores[3] = 65
+  fmt.Println(playerScores)
+}
