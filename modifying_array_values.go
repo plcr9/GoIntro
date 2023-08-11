760d1f2bf706740a0486937a552b234eccd2529e
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-11 11:05:30 +0100

    Update modifying_array_values.go

--- modifying_array_values.go
+++ modifying_array_values.go
@@ -1,1 +1,11 @@
+package main
 
+import "fmt"
+
+func main() {
+  myDogs := [3]string{"Frida", "Fedo", "Jegf"}
+  fmt.Println(myDogs)
+  myDogs[1] = "Fido"
+  myDogs[2] = "Jeff"
+  fmt.Println(myDogs)
+}
