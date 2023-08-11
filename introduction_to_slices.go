08e720285e1718f4a784b01beda141942e687cec
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-11 11:09:40 +0100

    Update introduction_to_slices.go

--- introduction_to_slices.go
+++ introduction_to_slices.go
@@ -1,1 +1,12 @@
+package main
 
+import "fmt"
+
+func main() {
+  myTutors := [4]string{"Kirsty", "Mishell", "Jose", "Neil"}
+  fmt.Println(myTutors)
+  myTutorSlice := myTutors[:]
+  mySubjects := []string{"Go", "Java", "Python"}
+  fmt.Println(myTutorSlice)
+  fmt.Println(mySubjects)
+}
