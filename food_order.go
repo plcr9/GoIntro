971515c3a778e364351e01d72c12a688a405ca2a
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-11 10:41:26 +0100

    Update food_order.go

--- food_order.go
+++ food_order.go
@@ -10,3 +10,22 @@ func askOrder() (string) {
   fmt.Scan(&input)
   return input
 }
+
+func main() {
+  fastfoodMenu := []string("Burgers", "Nuggets", "Fries"}
+  fmt.Println("The fast food menu has these items:", fastfoodMenu)
+
+  var total int
+  var order string
+
+  for order != "quit" {
+    order = askOrder()
+  }
+
+  fmt.Println("The total for the order is", total)
+}
+
+
+                           
+
+                      
