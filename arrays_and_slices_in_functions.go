3cfa16409fe2852b3848165197aa22ffc16aa3b5
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-11 11:36:10 +0100

    Update arrays_and_slices_in_functions.go

--- arrays_and_slices_in_functions.go
+++ arrays_and_slices_in_functions.go
@@ -1,1 +1,13 @@
+package main
 
+import "fmt"
+
+func main() {
+  myTutors := []string{"Kirsty", "Mishell", "Jose", "Neil"}
+  changeLastElement(myTutors, "Keith")
+}
+
+func changeLastElement(slice []string, newName string) {
+  slice[len(slice)-1] = newName
+  fmt.Println(slice)
+}
