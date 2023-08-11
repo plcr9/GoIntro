ed3df8dd1a9c8681e3eb4b632717e36b7621004d
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-11 11:47:47 +0100

    Update maps.go

--- maps.go
+++ maps.go
@@ -1,1 +1,13 @@
+package main
 
+import "fmt"
+
+func main() {
+  contacts := map[string]int{
+    "Joe": 2126778723,
+    "Angela": 4089978763,
+    "Shawn": 3143776876,
+    "Terell": 5026754531,
+  }
+  fmt.Println(contacts)
+}
