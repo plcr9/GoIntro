fc9e0e5186307ddee4a4c6e7deaa325155c24718
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-11 11:42:13 +0100

    Update array_summary.go

--- array_summary.go
+++ array_summary.go
@@ -1,1 +1,24 @@
+package main
 
+import "fmt"
+
+func printFirstLastArray(array[4]int) {
+  fmt.Println("First", array[0])
+  fmt.Println("Last", array[3])
+}
+
+func changeFirst(slice[]int, value int) {
+  if (len(slice) > 0) {
+    slice[0] = value
+  }
+}
+
+func main() {
+  myTutors := []string{"Kirsty", "Mishell", "Jose", "Neil"}
+  fmt.Println(myTutors)
+  theirHours := [4]int{20,10,30,15}
+  theirHoursSlice := theirHours[:]
+  changeFirst(theirHoursSlice, 25)
+  fmt.Println(theirHoursSlice)
+  printFirstLastArray(theirHours)
+}
