b55565b4988f1f0dc0d31c771ba333c1b8939cfe
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-12 12:25:22 +0100

    Update variables_and_functions.go

--- variables_and_functions.go
+++ variables_and_functions.go
@@ -1,1 +1,18 @@
+package main
 
+import "fmt"
+
+func main() {
+  fmt.Println("Hello, world.")
+
+  var whatToSay string
+  var i int
+
+  whatToSay = "This is the day I learn Golang!"
+
+  fmt.Println(whatToSay)
+
+  i = 7
+
+  fmt.Println("i is set to", i)
+}
