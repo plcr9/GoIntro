2cdd434d910d647a84f1f22b4420c9372f8a7528
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-12 11:36:41 +0100

    Update map_accessing_values.go

--- map_accessing_values.go
+++ map_accessing_values.go
@@ -1,1 +1,23 @@
+package main
+import "fmt"
 
+func main() {
+  donuts := map[string]int{
+    "frosted": 10,
+    "chocolate": 15,
+    "jelly": 8,
+  }
+
+  fmt.Println(donuts)
+
+  firstChoice := donuts["frosted"]
+  fmt.Println(firstChoice)
+
+  secondChoice,result := donuts["bavarian cream"]
+
+  if result {
+    fmt.Println(secondChoice)
+  } else {
+    fmt.Println("We do not sell that donut!")
+  }
+}
