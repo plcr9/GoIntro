82343155e1792190e12a1459e3878e3ddd356428
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-12 11:39:22 +0100

    Update maps_adding_and_modifying.go

--- maps_adding_and_modifying.go
+++ maps_adding_and_modifying.go
@@ -1,1 +1,19 @@
+package main
 
+import "fmt"
+
+func main() {
+  donuts := map[string]int{
+    "frosted": 10,
+    "chocolate": 15,
+    "jelly": 8,
+  }
+
+  fmt.Println(donuts)
+
+  donuts["glazed"] = 12
+  fmt.Println(donuts["glazed"])
+
+  donuts["jelly"] = 3
+  fmt.Println(donuts)
+}
