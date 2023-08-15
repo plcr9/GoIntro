80405cc9358a8af6b716e468799d588b819c72c0
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 14:20:23 +0100

    Update struct_nested.go

--- struct_nested.go
+++ struct_nested.go
@@ -1,1 +1,19 @@
+package main
 
+import "fmt"
+
+type Point struct {
+  x int
+  y int
+}
+
+type Circle struct {
+  Point
+  radius int
+}
+
+func main() {
+  circle := Circle{Point{4,5}, 2}
+  fmt.Println(circle.x)
+  fmt.Println(circle)
+}
