f407c864854888fec9e1055a8defe6ec80da4698
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 14:06:10 +0100

    Update struct_pointers.go

--- struct_pointers.go
+++ struct_pointers.go
@@ -1,1 +1,18 @@
+package main
 
+import "fmt"
+
+type Triangle struct {
+  height float32
+  base float32
+}
+
+func (tri *Triangle) updateBase(newBase float32){
+  tri.base = newBase
+}
+
+func main() {
+  triangle := Triangle{10, 4}
+  triangle.updateBase(13)
+  fmt.Println(triangle)
+}
