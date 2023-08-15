2efe03628aed0b455dcec018d0df9b55ca7b7d71
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 13:26:57 +0100

    Update struct_intro.go

--- struct_intro.go
+++ struct_intro.go
@@ -1,1 +1,18 @@
+package main
 
+import "fmt"
+
+type Pet struct {
+  name string
+  petType string
+  age int
+}
+
+func main() {
+  nuggets := Pet{"Nuggets", "dog", 4}
+  mittens := Pet{"Mittens", "cat", 7}
+  robin := Pet{"Robin", "bird", 2}
+  fmt.Println(nuggets)
+  fmt.Println(mittens)
+  fmt.Println(robin)
+}
