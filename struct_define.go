18558aebffeb60bdd458ea7b051057b2f4d9b9e2
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 13:29:12 +0100

    Update struct_define.go

--- struct_define.go
+++ struct_define.go
@@ -1,1 +1,15 @@
+package main
 
+import "fmt"
+
+type Country struct {
+  name string
+  capital string
+  latitude float32
+  longitude float32
+}
+
+func main() {
+  var france Country
+  fmt.Println(france)
+}
