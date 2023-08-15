25c97b32041a6961d41772ef74fb98af58cd85d5
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-15 14:03:31 +0100

    Update struct_functions.go

--- struct_functions.go
+++ struct_functions.go
@@ -1,1 +1,19 @@
+package main
 
+import "fmt"
+
+type Triangle struct {
+  height float32
+  base float32
+}
+
+func (triangle Triangle) area() float32 {
+  return 0.5 * (triangle.base * triangle.height)
+}
+
+func main() {
+  triangle := Triangle{10, 4}
+  fmt.Println(triangle)
+
+  fmt.Println(triangle.area())
+}
