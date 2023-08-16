afc900ff7dfcc3c23bccc04219a0d2dc38b4484d
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-16 10:53:13 +0100

    Update catnapped.go

--- catnapped.go
+++ catnapped.go
@@ -7,4 +7,5 @@ import (
 func main() {
   guests := [5]string{"Anna", "Jose", "Louis", "Tina", "Raymond"}
   objects := [4]string{"Toy Chest", "Vase", "Box", "Cat Tree"}
+  fmt.Println("Guests:", guests)
 }
