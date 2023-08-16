56f5832ebb56a917ecf6fa536b2984511aaadaef
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-16 10:52:44 +0100

    Update catnapped.go

--- catnapped.go
+++ catnapped.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+  "fmt"
+)
+
 func main() {
   guests := [5]string{"Anna", "Jose", "Louis", "Tina", "Raymond"}
   objects := [4]string{"Toy Chest", "Vase", "Box", "Cat Tree"}
