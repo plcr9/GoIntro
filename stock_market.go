4cd37f26edf5ec8158fa28360faa4fb144357f17
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-16 11:21:46 +0100

    Update stock_market.go

--- stock_market.go
+++ stock_market.go
@@ -10,6 +10,11 @@ func randomNumberGen(min float32, max float32) float32 {
   return min + (max-min)*rand.Float32()
 }
 
+type Stock struct{
+  name string
+  price float32
+}
+
 func main() {
   rand.Seed(time.Now().UnixNano())
 }
