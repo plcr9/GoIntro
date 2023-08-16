1e05d1b8289240d5bf9611b2bc1384423a4d76db
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-16 10:54:24 +0100

    Update catnapped.go

--- catnapped.go
+++ catnapped.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+  rand.Seed(time.Now().UnixNano())
   guests := [5]string{"Anna", "Jose", "Louis", "Tina", "Raymond"}
   objects := [4]string{"Toy Chest", "Vase", "Box", "Cat Tree"}
   fmt.Println("Guests:", guests)
