3785cb0f607596ed32c370311b1aa3a06f640284
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-01 13:21:02 +0100

    Update bulk_messages.go

--- bulk_messages.go
+++ bulk_messages.go
@@ -12,3 +12,13 @@ func bulkSend(numMessages int) float64 {
   return totalCost
 }
 
+func maxMessages(thresh float64) int {
+  totalCost := 0.0
+  for i := 0; i++ {
+    totalCost += 1.0 + (0.01 + float64(i))
+    if totalCost > thresh {
+      return i
+    }
+  }
+}
+
